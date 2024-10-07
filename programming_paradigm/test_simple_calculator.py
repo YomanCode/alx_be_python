946a5afc58ed5c0841a0e75fa84e45cd1ab4a24e
yomancode <yordiasfaw9@gmail.com>
2024-10-07 22:42:12 +0300

    test calc

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,19 +6,15 @@ class Test(unittest.TestCase):
         self.calc = SimpleCalculator()
 
     def test_addition(self):
-        add_result = self.calc.add(10, 5)
         self.assertEqual(self.calc.add(10, 5), 15)
     
     def test_subtraction(self):
-        sub_result = self.calc.subtract(10, 5)
         self.assertEqual(self.calc.subtract(10, 5), 5)
     
     def test_multiplication(self):
-        multiply_result = self.calc.multiply(10, 5)
         self.assertEqual(self.calc.multiply(10, 5), 50)
     
     def test_division(self):
-        division_result = self.calc.divide(10, 5)
         self.assertRaises(ZeroDivisionError)
         self.calc.divide(10, 0)
         self.assertEqual(self.calc.divide(10, 5), 2)
