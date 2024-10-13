1967abfc67ce0d484f575d3625f60e193d1d168e
yomancode <yordiasfaw9@gmail.com>
2024-10-13 19:24:40 +0300

    library system

--- oop/library_system.py
+++ oop/library_system.py
@@ -7,17 +7,17 @@ class Book:
         return f"Book: {self.title} by {self.author}"
 
 class EBook(Book):
-    def __init__(self, title, author, file_size: int):
+    def __init__(self, title, author, file_size):
         super().__init__(title, author)
-        self.file_size = file_size
+        self.file_size = int(file_size)
 
     def __str__(self):
         return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 class PrintBook(Book):
-    def __init__(self, title, author, page_count: int):
+    def __init__(self, title, author, page_count):
         super().__init__(title, author)
-        self.page_count = page_count
+        self.page_count = int(page_count)
 
     def __str__(self):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
